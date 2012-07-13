f36e877d2a4e0fb6c34d2a2cdc96b9b027bf7b68
Dmitry Malchikov <dmalch@gmail.com>
2012-07-13 19:38:37 +0400

    Implement apply patch method

--- src/test/java/com/github/dmalch/PatchIdePatcherTest.java
+++ src/test/java/com/github/dmalch/PatchIdePatcherTest.java
@@ -4,6 +4,7 @@ import com.google.common.io.Files;
 import de.schlichtherle.truezip.file.TFile;
 import de.schlichtherle.truezip.file.TFileReader;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Test;
 
 import java.io.File;
@@ -11,7 +12,9 @@ import java.io.IOException;
 import java.io.Reader;
 
 import static com.google.common.io.Closeables.closeQuietly;
+import static java.text.MessageFormat.format;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class PatchIdePatcherTest {
@@ -49,12 +52,22 @@ public class PatchIdePatcherTest {
     }
 
     private File givenExpectedFile() {
-        return new File("src/test/resources/file.txt");
+        final File originalFile = new File("src/test/resources/file.txt");
+        final File copiedDir = new File(format("out/test/{0}", RandomStringUtils.randomAlphanumeric(5)));
+        final File copiedFile = new File(copiedDir, format("file.txt", RandomStringUtils.randomAlphanumeric(5)));
+        try {
+            final boolean mkdirs = copiedDir.mkdirs();
+            assertThat(mkdirs, is(true));
+            Files.copy(originalFile, copiedFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return copiedFile;
     }
 
     private TFile givenZipFileToPatch() {
         final File originalFile = new File("src/test/resources/file.zip");
-        final File fileToPatch = new File("out/test/file_to_patch.zip");
+        final File fileToPatch = new File(format("out/test/file_to_patch{0}.zip", RandomStringUtils.randomAlphanumeric(5)));
         try {
             Files.copy(originalFile, fileToPatch);
         } catch (IOException e) {
