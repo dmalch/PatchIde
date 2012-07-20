6474ab44ecfe1230d8d14d4d71b66e7bdec6aaba
Dmitry Malchikov <dmalch@gmail.com>
2012-07-20 14:29:00 +0400

    Test patch several archives

--- src/test/java/com/github/dmalch/PatchIdePatcherTest.java
+++ src/test/java/com/github/dmalch/PatchIdePatcherTest.java
@@ -1,5 +1,7 @@
 package com.github.dmalch;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
 import de.schlichtherle.truezip.file.TFile;
@@ -9,9 +11,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 
+import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 
+import static com.google.common.collect.Collections2.transform;
 import static java.text.MessageFormat.format;
 import static org.apache.commons.lang.RandomStringUtils.randomAlphanumeric;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -39,7 +44,7 @@ public class PatchIdePatcherTest extends AbstractPatchTest {
         final String expectedDir = givenExpectedDir();
         final File expectedFile = givenPatchFile(expectedDir);
         final TFile zipFileToPatch = givenZipFileToPatch(expectedDir);
-        final PatchIdePatcher patcher = givenPatcherFor(expectedFile, zipFileToPatch, expectedDir, "");
+        final PatchIdePatcher patcher = givenPatcherFor(expectedFile, ImmutableList.of(zipFileToPatch), expectedDir, "");
 
         whenApplyPatch(patcher);
 
@@ -53,12 +58,39 @@ public class PatchIdePatcherTest extends AbstractPatchTest {
         final byte[] expectedFileValue = readFileContent(zipFileToPatch);
 
         final String fileMinRevision = "5";
-        final PatchIdePatcher patcher = givenPatcherFor(expectedFile, zipFileToPatch, "", fileMinRevision);
+        final PatchIdePatcher patcher = givenPatcherFor(expectedFile, ImmutableList.of(zipFileToPatch), "", fileMinRevision);
         when(revisionManager.isCurrentVersionGreaterThen(fileMinRevision)).thenReturn(false);
 
         whenApplyPatch(patcher);
 
         thenPatchIsNotApplied(zipFileToPatch, expectedFileValue);
+    }
+
+    @Test
+    public void testPatchIsAppliedWhenSeveralJarFilesAreProvidedForPatch() throws Exception {
+        final File expectedFile = givenPatchFile();
+        final TFile zipFileToPatch1 = givenZipFileToPatch();
+        final TFile zipFileToPatch2 = givenZipFileToPatch();
+
+        final PatchIdePatcher patcher = givenPatcherFor(expectedFile, zipFileToPatch1, zipFileToPatch2);
+
+        whenApplyPatch(patcher);
+
+        thenPatchIsApplied(zipFileToPatch1, expectedFile);
+        thenPatchIsApplied(zipFileToPatch2, expectedFile);
+    }
+
+    @Test
+    public void testPatchIsNotAppliedWhenJarFileDoesNotExist() throws Exception {
+        final File expectedFile = givenPatchFile();
+        final TFile zipFileToPatch = givenZipFileToPatch();
+
+        final PatchIdePatcher patcher = givenPatcherFor(expectedFile, zipFileToPatch);
+
+        whenZipFileDoesNotExist(zipFileToPatch);
+        whenApplyPatch(patcher);
+
+        nothingHappens();
     }
 
     @Test
@@ -165,16 +197,12 @@ public class PatchIdePatcherTest extends AbstractPatchTest {
         final TFile zipFileToPatch = givenZipFileToPatch();
 
         final String fileMinRevision = "5";
-        final PatchIdePatcher patcher = givenPatcherFor(patchFile, zipFileToPatch, "", fileMinRevision);
+        final PatchIdePatcher patcher = givenPatcherFor(patchFile, ImmutableList.of(zipFileToPatch), "", fileMinRevision);
         when(revisionManager.isCurrentVersionGreaterThen(fileMinRevision)).thenReturn(false);
 
         final boolean result = whenCheckFilesArePatched(patcher);
 
         thenFilesWerePatched(result);
-    }
-
-    private PatchIdePatcher givenPatcherFor(final File patchFile, final TFile zipFileToPatch) {
-        return givenPatcherFor(patchFile, zipFileToPatch, "", "");
     }
 
     private TFile givenZipFileToPatch() {
@@ -238,6 +266,14 @@ public class PatchIdePatcherTest extends AbstractPatchTest {
         assertThat(fileToPatchValue, equalTo(expectedFileValue));
     }
 
+    private PatchIdePatcher givenPatcherFor(final File patchFile, final TFile zipFileToPatch) {
+        return givenPatcherFor(patchFile, ImmutableList.of(zipFileToPatch), "", "");
+    }
+
+    private PatchIdePatcher givenPatcherFor(final File patchFile, final TFile zipFileToPatch1, final TFile zipFileToPatch2) {
+        return givenPatcherFor(patchFile, ImmutableList.of(zipFileToPatch1, zipFileToPatch2), "", "");
+    }
+
     private TFile givenZipFileToPatch(final String expectedDir) {
         final File originalFile = new File("src/test/resources/file.zip");
         final File fileToPatch = new File(format("out/test/file_to_patch{0}.zip", randomAlphanumeric(5)));
@@ -250,20 +286,42 @@ public class PatchIdePatcherTest extends AbstractPatchTest {
         return new TFile(fileToPatch, format("{0}/file.txt", expectedDir));
     }
 
-    private PatchIdePatcher givenPatcherFor(final File expectedFile, final TFile zipFileToPatch, final String innerDirectory, final String minimalRevision) {
-        idePatcher.setFilesToPatch(ImmutableMap.of(expectedFile.getAbsolutePath(), patchTarget(zipFileToPatch.getInnerArchive().getAbsolutePath(), innerDirectory, minimalRevision)));
+    private PatchIdePatcher givenPatcherFor(final File expectedFile, final Collection<TFile> zipFilesToPatch, final String innerDirectory, final String minimalRevision) {
+        idePatcher.setFilesToPatch(ImmutableMap.of(expectedFile.getAbsolutePath(), patchTarget(innerDirectory, transform(zipFilesToPatch, extractArchivePath()), minimalRevision)));
         new File("out/test/file.txt.bak").delete();
 
         when(revisionManager.isCurrentVersionGreaterThen(minimalRevision)).thenReturn(true);
         return idePatcher;
     }
 
-    private PatchTarget patchTarget(final String pathToArchive, final String innerDir, final String minimalRevision) {
+    private Function<TFile, String> extractArchivePath() {
+        return new Function<TFile, String>() {
+            @Override
+            public String apply(@Nullable final TFile zipFilesToPatch) {
+                return zipFilesToPatch.getInnerArchive().getAbsolutePath();
+            }
+        };
+    }
+
+    private PatchTarget patchTarget(final String innerDir, final String pathToArchive, final String minimalRevision) {
         return new PatchTarget(innerDir, pathToArchive, minimalRevision);
+    }
+
+
+    private PatchTarget patchTarget(final String innerDir, final Collection<String> pathToArchives, final String minimalRevision) {
+        return new PatchTarget(innerDir, pathToArchives, minimalRevision);
     }
 
     private void whenApplyPatch(final PatchIdePatcher patcher) {
         patcher.applyPatch();
     }
+
+    private void nothingHappens() {
+    }
+
+    private void whenZipFileDoesNotExist(final TFile zipFileToPatch) throws IOException {
+        zipFileToPatch.rm();
+    }
+
 }
 
