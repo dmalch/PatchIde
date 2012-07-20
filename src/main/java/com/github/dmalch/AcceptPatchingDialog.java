ded4c1d62be153643b7b3ffb82a94c837613c1c0
Dmitry Malchikov <dmalch@gmail.com>
2012-07-20 19:14:30 +0400

    Fix text

--- src/main/java/com/github/dmalch/AcceptPatchingDialog.java
+++ src/main/java/com/github/dmalch/AcceptPatchingDialog.java
@@ -5,7 +5,7 @@ import com.intellij.openapi.ui.Messages;
 public class AcceptPatchingDialog {
 
     public int showDialog() {
-        return Messages.showYesNoDialog("Patch IDE plugin wants to patch the instance of your IDE", "Confirm Patch",
+        return Messages.showYesNoDialog("Patch IDE plugin wants to patch the instance of your IDE. You can revert or apply patch at settings/Patch IDE menu.", "Confirm Patch",
                 "Patch", "Cancel",
                 Messages.getQuestionIcon());
     }
